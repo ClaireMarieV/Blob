f5396e34005212c2857b7fb70711532e3701c280
Claire Vaney <clairevan.54.cv@gmail.com>
2020-12-08 14:54:57 +0100

    created lines cubic bezier but bug with control points

--- components/Blob.js
+++ components/Blob.js
@@ -20,17 +20,21 @@ const Blob = ({ pointCount, color }) => {
       i % 2 === 0 ? Math.random() * (1 - minOffset) + minOffset : 1;
 
     points.push({
-      control1: {
+      startControl: {
         x: round(
           -control * sin(startAngle) + radius * cos(startAngle) * offset
         ),
         y: round(control * cos(startAngle) + radius * sin(startAngle) * offset),
       },
-      control2: {
+      endControl: {
         x: round(control * sin(endAngle) + radius * cos(endAngle) * offset),
         y: round(-control * cos(endAngle) + radius * sin(endAngle) * offset),
       },
-      point: {
+      startPoint: {
+        x: round(Math.cos(startAngle) * offset),
+        y: round(Math.sin(startAngle) * offset),
+      },
+      endPoint: {
         x: round(Math.cos(endAngle) * offset),
         y: round(Math.sin(endAngle) * offset),
       },
@@ -41,19 +45,19 @@ const Blob = ({ pointCount, color }) => {
     "M1 0 " +
     points
       .map(
-        ({ control1, control2, point }) =>
+        ({ startControl, endControl, endPoint }) =>
           "C" +
-          control1.x +
+          startControl.x +
           " " +
-          control1.y +
+          startControl.y +
           ", " +
-          control2.x +
+          endControl.x +
           " " +
-          control2.y +
+          endControl.y +
           ", " +
-          point.x +
+          endPoint.x +
           " " +
-          point.y
+          endPoint.y
       )
       .join(" ") +
     "Z";
@@ -62,19 +66,44 @@ const Blob = ({ pointCount, color }) => {
     <div>
       <svg viewBox="-2 -2 4 4">
         <path fill={color} d={path}></path>
-        {points.map(({ point }) => (
-          <circle cx={point.x} cy={point.y} r="0.02"></circle>
+        {points.map(({ endPoint }) => (
+          <circle cx={endPoint.x} cy={endPoint.y} r="0.02"></circle>
         ))}
-        {points.map(({ control1 }) => (
+        {points.map(({ startPoint, startControl }) => (
+          <line
+            x1={startPoint.x}
+            y1={startPoint.y}
+            x2={startControl.x}
+            y2={startControl.y}
+            stroke="black"
+            stroke-width="0.01"
+          ></line>
+        ))}
+        {points.map(({ startControl }) => (
           <circle
-            cx={control1.x}
-            cy={control1.y}
+            cx={startControl.x}
+            cy={startControl.y}
             r="0.02"
             fill="orange"
           ></circle>
         ))}
-        {points.map(({ control2 }) => (
-          <circle cx={control2.x} cy={control2.y} r="0.02" fill="gold"></circle>
+        {points.map(({ endPoint, endControl }) => (
+          <line
+            x1={endPoint.x}
+            y1={endPoint.y}
+            x2={endControl.x}
+            y2={endControl.y}
+            stroke="black"
+            stroke-width="0.01"
+          ></line>
+        ))}
+        {points.map(({ endControl }) => (
+          <circle
+            cx={endControl.x}
+            cy={endControl.y}
+            r="0.02"
+            fill="gold"
+          ></circle>
         ))}
       </svg>
       <style jsx>{`
