b327183031e1126ba708549e9fc6a62e6391f389
Claire Vaney <clairevan.54.cv@gmail.com>
2020-12-08 09:44:10 +0100

    added component blob and import this into index

---
+++ components/Blob.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const round = (value) => Math.round(value * 10000000) / 10000000;
+const sin = Math.sin;
+const cos = Math.cos;
+
+const Blob = ({ pointCount }) => {
+  const radius = 1;
+  const angle = (2 * Math.PI) / pointCount;
+  let index = 0;
+  let path = "M1 0";
+  const points = [];
+  const control =
+    (4 / 3) * Math.tan(Math.PI / (2 * (1 / (angle / (Math.PI * 2))))) * radius;
+
+  for (let i = 0; i < pointCount; i++) {
+    const startAngle = angle * i;
+    const endAngle = startAngle + angle;
+
+    path += " C";
+    path += round(-control * sin(startAngle) + radius * cos(startAngle));
+    path += " ";
+    path += round(control * cos(startAngle) + radius * sin(startAngle));
+    path += ", ";
+    path += round(control * sin(endAngle) + radius * cos(endAngle));
+    path += " ";
+    path += round(-control * cos(endAngle) + radius * sin(endAngle));
+    path += ", ";
+    path += round(Math.cos(endAngle));
+    path += " ";
+    path += round(Math.sin(endAngle));
+
+    points.push({ x: Math.cos(startAngle), y: Math.sin(startAngle) });
+  }
+  path += "z";
+  return (
+    <div>
+      <svg viewBox="-2 -2 4 4">
+        {points.map((point) => (
+          <circle cx={point.x} cy={point.y} r="0.01"></circle>
+        ))}
+        <path fill="black" d={path}></path>
+      </svg>
+      <style jsx>{`
+        svg {
+          height: 100vh;
+        }
+      `}</style>
+    </div>
+  );
+};
+export default Blob;
--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,5 @@
-import Head from "next/head";
+import React from "react";
+import Blob from "../components/Blob.js";
 
-const round = (value) => Math.round(value * 10000000) / 10000000;
-
-const sin = Math.sin;
-const cos = Math.cos;
-
-export default function Home() {
-  const pointCount = 8;
-  const radius = 1;
-  const angle = (2 * Math.PI) / pointCount;
-
-  let path = "M1 0";
-  const points = [];
-  const control =
-    (4 / 3) * Math.tan(Math.PI / (2 * (1 / (angle / (Math.PI * 2))))) * radius;
-  for (let i = 0; i < pointCount; i++) {
-    const startAngle = angle * i;
-    const endAngle = startAngle + angle;
-
-    path += " C";
-    path += round(-control * sin(startAngle) + radius * cos(startAngle));
-    path += " ";
-    path += round(control * cos(startAngle) + radius * sin(startAngle));
-    path += ", ";
-    path += round(control * sin(endAngle) + radius * cos(endAngle));
-    path += " ";
-    path += round(-control * cos(endAngle) + radius * sin(endAngle));
-    path += ", ";
-    path += round(Math.cos(endAngle));
-    path += " ";
-    path += round(Math.sin(endAngle));
-
-    points.push({ x: Math.cos(startAngle), y: Math.sin(startAngle) });
-  }
-  path += "z";
-
-  return (
-    <div>
-      <svg viewBox="-2 -2 4 4">
-        {points.map((point) => (
-          <circle cx={point.x} cy={point.y} r="0.01"></circle>
-        ))}
-        <path fill="black" d={path}></path>
-      </svg>
-      <style jsx>{`
-        svg {
-          height: 100vh;
-        }
-      `}</style>
-    </div>
-  );
-}
+const IndexPage = () => <Blob pointCount="8" />;
+export default IndexPage;
