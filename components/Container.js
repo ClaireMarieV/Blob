d95cca9e1f1606f4d4e9e5dca50d227814158104
Claire Vaney <clairevan.54.cv@gmail.com>
2021-01-12 15:53:00 +0100

    added grid column 2

--- components/Container.js
+++ components/Container.js
@@ -2,17 +2,13 @@ import React from "react";
 
 const Container = ({ children }) => (
   <div className="container">
-    <div className="title">
-      <h1>ORGANIC BLOB</h1>
-    </div>
     {children}
     <style jsx>{`
       .container {
-        margin: 1rem;
-      }
-      .container .title {
-        display: flex;
-        justify-content: flex-start;
+        width: 100%;
+        display: grid;
+        margin-top: 2rem;
+        grid-template-columns: repeat(2, minmax(0, 1fr));
       }
     `}</style>
   </div>
