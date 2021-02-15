ed40f1882c88c74e7bb0150fe19513107d2c7107
ClaireMarieV <>
2021-02-15 11:16:52 +0100

    added component who download svg

---
+++ components/DownloadBlob.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const DownloadBlob = ({ points, color }) => {
+  const path =
+    "M1 0 " +
+    points
+      .map(
+        ({ startControl, endControl, endPoint }) =>
+          "C" +
+          startControl.x +
+          " " +
+          startControl.y +
+          ", " +
+          endControl.x +
+          " " +
+          endControl.y +
+          ", " +
+          endPoint.x +
+          " " +
+          endPoint.y
+      )
+      .join(" ") +
+    "Z";
+
+  return (
+    <a
+      download="blob.svg"
+      href={
+        typeof btoa !== "undefined" &&
+        "data:image/svg+xml;base64," +
+          btoa(
+            "<?xml version='1.0' standalone='no'?><svg viewBox='-1 -1 2 2' xmlns='http://www.w3.org/2000/svg'><path fill='" +
+              color +
+              "' d='" +
+              path +
+              "'></path></svg>"
+          )
+      }
+    >
+      <button>
+        <img src="Download1.svg" />
+      </button>
+      <style jsx>{`
+        button {
+          background: transparent;
+          border: 3px solid #a1d3b8;
+        }
+      `}</style>
+    </a>
+  );
+};
+export default DownloadBlob;
